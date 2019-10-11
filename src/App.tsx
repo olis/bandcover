a046556373737a4bdf7da7f9f9bb0edd442f5f7a
Schemmel Oliver <aeos@vfst.int>
2019-10-11 13:50:22 +0200

    css stylings on cards

--- src/App.tsx
+++ src/App.tsx
@@ -8,22 +8,24 @@ const App: React.FC = () => {
   for (let i = 1; i <= 10; i++) {
     cards.push(
       <div key={i} className="col-md-4">
-        <div className="card">
+        <div className="card mb-4 shadow-sm">
           <div className="card-body">
             <h5 className="card-title">Song {i}</h5>
             <h6 className="card-subtitle">as performed by Group{i}</h6>
-            <ul className="list-group">
-              <li className="list-group-item">Key: C#</li>
-              <li className="list-group-item">BPM: 120</li>
-            </ul>
-            <div className="card-body">
-              <p className="card-text">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque
-                nesciunt iusto pariatur assumenda, suscipit repellat molestias,
-                quasi porro distinctio voluptatum impedit repellendus fugiat
-                vel! Cumque libero odit quo dolorum laborum!
-              </p>
-            </div>
+          </div>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">
+              Key: <span>C#</span>
+            </li>
+            <li className="list-group-item">BPM: 120</li>
+          </ul>
+          <div className="card-body">
+            <p className="card-text">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque
+              nesciunt iusto pariatur assumenda, suscipit repellat molestias,
+              quasi porro distinctio voluptatum impedit repellendus fugiat vel!
+              Cumque libero odit quo dolorum laborum!
+            </p>
           </div>
         </div>
       </div>
@@ -39,7 +41,7 @@ const App: React.FC = () => {
             <h1>Song * Cover</h1>
           </div>
         </section>
-        <div className="album">
+        <div className="py-5">
           <div className="container">
             <div className="row">{cards}</div>
           </div>
