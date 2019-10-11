c543635b8fed3378f8e4d8a7234b88c6d15bf437
Schemmel Oliver <aeos@vfst.int>
2019-10-11 13:21:55 +0200

    songs rendered as cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 const App: React.FC = () => {
+  const cards = [];
+
+  for (let i = 1; i <= 10; i++) {
+    cards.push(
+      <div key={i} className="col-md-4">
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">Song {i}</h5>
+            <h6 className="card-subtitle">as performed by Group{i}</h6>
+            <ul className="list-group">
+              <li className="list-group-item">Key: C#</li>
+              <li className="list-group-item">BPM: 120</li>
+            </ul>
+            <div className="card-body">
+              <p className="card-text">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque
+                nesciunt iusto pariatur assumenda, suscipit repellat molestias,
+                quasi porro distinctio voluptatum impedit repellendus fugiat
+                vel! Cumque libero odit quo dolorum laborum!
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
-	  <main>
-		  <section className="jumbotron text-center">
-			  <h1>Song * Cover</h1>
-		  </section>
-	  </main>
+      <header className="App-header"></header>
+      <main>
+        <section className="jumbotron text-center">
+          <div className="container">
+            <h1>Song * Cover</h1>
+          </div>
+        </section>
+        <div className="album">
+          <div className="container">
+            <div className="row">{cards}</div>
+          </div>
+        </div>
+      </main>
     </div>
   );
-}
+};
 
 export default App;
