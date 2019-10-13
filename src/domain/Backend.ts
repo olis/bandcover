3e1ed6b09ac97dff857166e7d3cd2447124d1f4f
Schemmel Oliver <aeos@vfst.int>
2019-10-13 18:00:43 +0200

    heroku backend now default

--- src/domain/Backend.ts
+++ src/domain/Backend.ts
@@ -1,6 +1,6 @@
 const Backend = {
   getURL: () => {
-    if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+    if (process.env.NODE_ENV && process.env.NODE_ENV === "development") {
       return "http://localhost:5000";
     } else {
       return "https://songcover-backend.herokuapp.com";
