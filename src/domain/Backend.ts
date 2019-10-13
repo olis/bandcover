173ebbef8d9de1a1f000c8f140c77dba4d23a97d
Schemmel Oliver <aeos@vfst.int>
2019-10-13 18:09:55 +0200

    Notlösung: heroku backend fest

--- src/domain/Backend.ts
+++ src/domain/Backend.ts
@@ -1,10 +1,10 @@
 const Backend = {
   getURL: () => {
-    if (process.env.NODE_ENV && process.env.NODE_ENV === "development") {
-      return "http://localhost:5000";
-    } else {
-      return "https://songcover-backend.herokuapp.com";
-    }
+    // if (process.env.NODE_ENV && process.env.NODE_ENV === "development") {
+    //   return "http://localhost:5000";
+    // } else {
+    return "https://songcover-backend.herokuapp.com";
+    // }
   }
 };
 
