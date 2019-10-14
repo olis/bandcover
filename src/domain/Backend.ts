74235e9855754a7e1feb5b73d1f1bbb4564db236
Oliver Schemmel <oliver.schemmel@web.de>
2019-10-14 23:03:57 +0200

    trying to set environment with .env file

--- src/domain/Backend.ts
+++ src/domain/Backend.ts
@@ -1,9 +1,9 @@
 const Backend = {
   getURL: () => {
-    if (process.env.NODE_ENV && process.env.NODE_ENV === "development") {
-      return "http://localhost:5000";
+    if (process.env && process.env.BACKEND_URL) {
+      return process.env.BACKEND_URL;
     } else {
-      return "https://songcover-backend.herokuapp.com";
+      return "http://localhost:5000";
     }
   }
 };
